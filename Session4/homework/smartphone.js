e09e70b8a3450014b883aa03c248eef2fc903ae4
truongan2000pro <kingdragon7a8@gmail.com>
2019-11-02 00:27:20 +0700

    initial commit

--- Session4/homework/smartphone.js
+++ Session4/homework/smartphone.js
@@ -5,14 +5,14 @@ var product1={
     color:"white",
     category: "phone"
 }
-product2={
+var product2={
     name:"samsung charger",
     brand:"samsung",
     price:200,
     color:"black",
     category:"charger"
 }
-product3={
+var product3={
     name:"Iphone X",
     brand:"Apple",
     price:600,
@@ -24,34 +24,29 @@ var array = products
 var dunno=["name","price"]
 var dunno1=["category"]
  for (let i=0;i<array.length;i++){
-for(let x in dunno){
-console.log(`${i+1} ${dunno[x]}: ${array[i][dunno[x]]}`)
-}
+console.log(`name: ${array[i].name} \nprice: ${array[i].price} `)
 console.log("----------------------------------")
 }  
 let v=0
 while(v<2){
-let userInput=prompt("Nhap vao Category ban muon xem")
+let userInput=prompt("Nhap vao Category ban muon xem và ấn E để thoát").toLowerCase()
+if(userInput ==="e"){
+    break
+}  
 for(let i=0;i<array.length;i++){
         var {name,brand,price,color,category} = array[i]
-if(userInput == category){
+        if(userInput === category){
         for(let x in dunno){
             console.log(`${dunno[x]}: ${array[i][dunno[x]]}`)
             }
             console.log("-------------------------------------")
         }
+         
         else if(userInput != "charger" && userInput != "phone" ){
             alert("Nhập không đúng rồi nhập lại đi!!!!!")
             break
         }
+       
     }
 }
 
-
-//var value=Object.values(array[1])
-// if(userInput == "charger" ){
-    
-// }
-// else{
-//     console.log("dcm")
-// }
---
+++ Session4/homework/smartphone2.js
@@ -0,0 +1,45 @@
+var product1={
+    name:"xiaomi note 4",
+    brand: "xiaomi",
+    price:400,
+    color:"white",
+    category: "phone"
+}
+var product2={
+    name:"samsung charger",
+    brand:"samsung",
+    price:200,
+    color:"black",
+    category:"charger"
+}
+var product3={
+    name:"Iphone X",
+    brand:"Apple",
+    price:600,
+    color:"pink",
+    category:"phone"
+}
+var products=[product1,product2,product3]
+product1.providers = ["TGDD","FPT"]
+product2.providers = ["Media"]
+product3.providers = ["AN","FPT"]
+var array = products
+var dunno=["name","price"]
+var dunno1=["category"]
+ for (let i=0;i<array.length;i++){
+console.log(`#${i+1}.${array[i].name} \nprice: ${array[i].price} \nproviders: ${array[i].providers} `)
+console.log("----------------------------------")
+}
+console.log(Object.values(product1))
+// var inputProvider=prompt("Nhập nhà cung cấp điện thoại bạn muốn xem: ")
+
+
+// let userInput=parseInt(prompt("Nhập vào vị trí bạn muốn xem thông tin điện thoại: "))
+// if(userInput - 1 >= products.length || userInput -1 <0){
+//     alert("sai vị trí rồi !!!!")
+// }
+// else if(userInput -1 <products.length && userInput-1>0){
+//     var {name,brand,price,color,category} = array[userInput-1]
+//     console.log(`Name:${name}\nBrand:${brand}\nPrice:${price}\nColor:${color}\nCategory:${category}`)
+// }
+
