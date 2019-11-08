a1f82adebf3421960cc537cfa8f807e9fecc8946
truongan2000pro <kingdragon7a8@gmail.com>
2019-11-09 00:52:32 +0700

    initial commit

--- Session6/homework/bai10+11.js
+++ Session6/homework/bai10+11.js
@@ -2,6 +2,9 @@ const is_inside = (a,b) => {
   if(a[0] > b[0] && a[1] > b[1]){
     return true
   }
+  else if(a[0] > b[0]+b[2] || a[1] > b[1]+b[3]){
+    return false
+  } 
   else{
       return false
   }
